cb99bacc02ac0bbfa20d2cd782a99675466b1f58
Valentin <valentyndemian@gmail.com>
2026-02-11 19:25:04 +0200

    setup routing between Home, Cabinet and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CarCard from './components/CarCard';
+
+import Home from './pages/Home';
+import Cabinet from './pages/Cabinet';
+import About from './pages/About';
 
 function App() {
-    const testCar = {
-        id: 1,
-        brand: "BMW",
-        model: "X5",
-        price: 3000,
-        image: "BMW X5 Image",
-        transmission: "Автомат",
-        rating: 5.0
-    };
-
-    const handleRent = (car) => {
-        alert(`Ви хочете орендувати: ${car.brand} ${car.model}`);
-    };
-
     return (
         <>
             <Header />
 
             <main className="container">
-                <section>
-                    <h2>Наш автопарк (Тест React)</h2>
-                    <div className="car-grid">
-                        <CarCard car={testCar} onRent={handleRent} />
-                        <CarCard car={testCar} onRent={handleRent} />
-                        <CarCard car={testCar} onRent={handleRent} />
-                    </div>
-                </section>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/cabinet" element={<Cabinet />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
             </main>
 
             <Footer />
